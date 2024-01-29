d3f031a7e64496f570f1cf15bbf6b48f9d4be1ee
yxiang921 <xiangf35@gmail.com>
2024-01-29 16:28:55 +0800

    Update Project

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -11,7 +11,7 @@ const Cart = () => {
 
   useEffect(() => {
     let total = 0;
-    cart.map((item) => {
+    cart.map((item, key) => {
       total += (item.unitPrice * item.quantity);
     })
     setTotalPrice(total);
